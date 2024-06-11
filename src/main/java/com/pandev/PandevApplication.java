c76b942053eb12e29b787322ff91b363239d78e8
Vladimir <surorion@mail.ru>
2024-06-11 16:26:39 +0300

    baseCommit

---
+++ src/main/java/com/pandev/PandevApplication.java
@@ -0,0 +1,13 @@
+package com.pandev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandev/PandevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
