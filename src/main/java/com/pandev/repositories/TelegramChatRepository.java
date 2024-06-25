66c10e5c39c25b51527708dcadddae3f8e51a8a0
Vladimir <surorion@mail.ru>
2024-06-25 14:55:39 +0300

    delete_TelegramChatRepo

--- src/main/java/com/pandev/repositories/TelegramChatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pandev.repositories;
-
-import com.pandev.entities.TelegramChat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TelegramChatRepository extends JpaRepository<TelegramChat, Integer> {
-    TelegramChat findByChatId(long chatId);
-}
